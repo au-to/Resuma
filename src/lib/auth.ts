dbeca5c67270e1f9e1a66b958aa4130204592613
Ryan <2996442165@qq.com>
2025-08-02 17:13:29 +0800

    fix

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -6,14 +6,7 @@ import type { NextAuthConfig } from 'next-auth';
 const config = {
   providers: [
     EmailProvider({
-      server: {
-        host: process.env.EMAIL_SERVER_HOST,
-        port: process.env.EMAIL_SERVER_PORT,
-        auth: {
-          user: process.env.EMAIL_SERVER_USER,
-          pass: process.env.EMAIL_SERVER_PASSWORD,
-        },
-      },
+      server: process.env.EMAIL_SERVER,
       from: process.env.EMAIL_FROM,
     }),
   ],
